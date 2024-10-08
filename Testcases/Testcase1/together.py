769797f1cefd00f6236fbf23d6e0ae2812210458
ZhenmeiOng <zhenmeiong@gmail.com>
2024-10-08 19:27:01 -0400

    modified together.py to adapt faasd

--- Testcases/Testcase1/together.py
+++ Testcases/Testcase1/together.py
@@ -3,27 +3,26 @@ need to remove the AWS-specific parts (like the boto3 library) and restructure
 the code to handle HTTP requests using a framework like Flask. This will allow 
 you to deploy it as a serverless function in faasd.'''
 
+## handler.py for together 
 
-import boto3
+
 import time
 import os
 import random
 from multiprocessing import Process, Pipe
 
-bucketName = "resource-efficient"
 defaultKey = "loopTime.txt"
 defaultLoopTime = 10000000
 defaultParallelIndex = 100
 
-def lambda_handler(event, context):
+def handler(event, context):
     startTime = GetTime()
     if 'key' in event:
         key = event['key']
     else:
         key = defaultKey
 
-    download_file(key)
-    loopTime = extractLoopTime(key)
+    loopTime = extractLoopTime(key)  # key is the file path?
 
     retTime = GetTime()
     result1 = {
@@ -35,22 +34,15 @@ def lambda_handler(event, context):
     }
     return alu_handler(result1,"")
 
-
-def download_file(key):
-    filepath = "/tmp/%s" %key
-
-    s3 = boto3.client('s3')
-    with open(filepath, 'wb+') as f:
-        s3.download_fileobj(bucketName, key, f)
-
-
-def extractLoopTime(key):
-    filepath = "/tmp/%s" %key
-    txtfile = open(filepath, 'r')
-    loopTime = int(txtfile.readline())
-    print("loopTime: " + str(loopTime))
-    txtfile.close()
-    return loopTime
+def extractLoopTime(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            loopTime = int(f.readline().strip())
+            print("loopTime: " + str(loopTime))
+            return loopTime
+    except Exception as e:
+        print(f"Error reading loop time from file: {e}")
+        return defaultLoopTime
 
 def alu_handler(event, context):
     startTime = GetTime()
